7b43591685433eea153235f6320de89c3d65ec23
shahbaz <mohdshahbaz@gmail.com>
2021-07-09 12:19:19 +0530

    fixed syles

--- src/components/Course.js
+++ src/components/Course.js
@@ -5,13 +5,15 @@ import data from '../data/db';
 export const Course = () => {
 
     const { id } = useParams();
-    const course = data.courses[id-1];
+    const course = data.courses[id - 1];
 
     return (
-        <div className="container card p-5 my-4">
-            <h2>{course.title}</h2>
-            <p><strong>Desc: </strong>{course.description}</p>
-            <p><strong>Author: </strong>{course.author}</p>
+        <div className="container my-4">
+            <div className="card p-5">
+                <h2>{course.title}</h2>
+                <p><strong>Desc: </strong>{course.description}</p>
+                <p><strong>Author: </strong>{course.author}</p>
+            </div>
         </div>
     )
 }
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -9,8 +9,8 @@ export const Courses = () => {
         <div className="container my-4">
             {data.courses.map((course) => {
                 return (
-                    <Link className="text-decoration-none text-reset" to={`/course/${course.id}`}>
-                        <div className="card my-3 p-5" key={course.id}>
+                    <Link className="text-decoration-none text-reset"  key={course.id} to={`/course/${course.id}`}>
+                        <div className="card my-3 p-5">
                             <h2>{course.title}</h2>
                             {/* <p><strong>Author: </strong>{course.author}</p> */}
                         </div>
